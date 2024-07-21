a9281354536515ec1b094c22570f0695ddafb676
Kristina Faye <hellokristinafaye@gmail.com>
2024-07-20 21:57:22 -0700

    practicing JS from JS.info inside this script file

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-alert("success")
+// alert("success")
+
+// let sum = (a, b) => a * b;
+
+// /* This arrow function is a shorter form of:
+
+// let sum = function(a, b) {
+//   return a + b;
+// };
+// */
+
+// alert(sum(13, 12)); // 3
+
+let double = n => n * 2;
+// roughly the same as: let double = function(n) { return n * 2 }
+
+alert(double(23)); // 46
+
